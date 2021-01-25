68b73a2910ce8c263ef1b739c2f2e63168cc4b10
ClaireMah <00.clairebear@gmail.com>
2021-01-24 18:06:56 -0700

    LSA class. placeholders in A and w functions

--- Lab1/LSA.cpp
+++ Lab1/LSA.cpp
@@ -1,1 +1,63 @@
 #include "LSA.h"
+
+void LSA::adjustment()
+{
+    N_iter++;
+	cout << "~~~~~~~~ ITERATION #" << N_iter << " ~~~~~~~~~~\n";
+
+    //read observations, etc
+    //populate x_o, l_o,
+	this->designA();
+	this->misclosure();
+	this->weights();
+
+	N = A.transpose() * P * A;
+	U = A.transpose() * P * w;
+	delta = -1 * N.inverse() * U;
+	v = A * delta + w;
+
+	x_cap = x_o + delta;
+	l_cap = l_o + v;
+
+
+	if (N_iter < 5)
+	{
+		for (int i = 0; i < delta.rows(); ++i)
+		{
+			if (this->delta(i, 0) > 0.00001 || this->delta(i, 0) < -0.00001)
+			{
+				l_o = l_cap;
+				x_o = x_cap;
+				adjustment();
+			}
+		}
+	}
+}
+
+void LSA::designA()
+{
+    A.resize(N_sat, 4);
+    for(int i = 0; i<A.rows(); ++i)
+    {
+        for(int j=0; j<A.cols()-1; ++j)
+        {
+            A(i,j) = 3; //TODO: wait for format of coordinates, etc.
+        }
+        A(i,A.cols()) = -1;
+    }
+}
+
+void LSA::misclosure()
+{
+    w.resize(N_sat, 1);
+    for(int i = 0; i<w.rows(); ++i)
+    {
+        w(i,0) = 6; //TODO: wait for format of coordinates, etc.
+    }
+}
+
+void LSA::weights()
+{
+    C_l.Identity(N_sat, N_sat);
+    P = apriori*C_l.inverse();
+}
--- Lab1/LSA.h
+++ Lab1/LSA.h
@@ -4,12 +4,58 @@
 #include <Eigen/Dense>
 #include <iomanip>
 #include <string>
+#include <iostream>
 
 using namespace Eigen;
+using namespace std;
 
 class LSA {
 	public:
 
+    int N_sat;          //number of satellites/number of observations
+    int N_iter=0;       //number of iterations
 
+    MatrixXd l_o;       //pseudorange observations
+    MatrixXd x_o;       //initial estimate. Parameters x, y, z, t
+    MatrixXd C_l;       //variance matrix of observations
+    MatrixXd P;         //weight matrix
+    double apriori=1;   //apriori variance factor
+    MatrixXd N;         //normal equations matrix
+    MatrixXd U;         //intermediate calculation
+    MatrixXd x_cap;     //corrected parameters
+    MatrixXd l_cap;     //corrected observations
+    MatrixXd delta;     //correction vector
+    MatrixXd A;         //design matrix
+    MatrixXd w;         //misclosure vector
+    MatrixXd C_xcap;    //variance covariance of estimate
+    MatrixXd v;         //residuals
 
+    /*Function: adjustment
+    Runs an iterative least squares adjustment
+    Inputs: None
+    Outputs: None
+    */
+    void adjustment();
+
+    /*Function: designA
+    Populates design matrix, A
+    Inputs: None
+    Outputs: None
+    */
+    void designA();
+
+    /*Function: misclosure
+    Populates misclosure vector
+    Inputs: None
+    Outputs: None
+    */
+    void misclosure();
+
+    /*Function: weights
+    Populates variance matrix of observations and weight matrix
+    Assumes uniform variances for all observations
+    Inputs: None
+    Outputs: None
+    */
+    void weights();
 };
